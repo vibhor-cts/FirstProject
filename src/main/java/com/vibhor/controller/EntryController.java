5d51c50050861d56ff0edf0cade8d2730edf7d00
vibhor <vibhor.mahajan@cognizant.com>
2013-11-18 11:08:00 +0530

    First Commit

---
+++ src/main/java/com/vibhor/controller/EntryController.java
@@ -0,0 +1,37 @@
+package com.vibhor.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.vibhor.entities.User;
+
+@Controller
+public class EntryController {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String showEntryForm(Model model){
+		model.addAttribute("user",getUser());
+		return "entryForm";
+	}
+	
+	@RequestMapping(value="/entry", method=RequestMethod.POST)
+	public String showResults(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+		if(bindingResult.hasErrors()){
+			System.out.println("in error part");
+			return "entryForm";
+		}
+		return "results";
+	}
+	
+	@ModelAttribute
+	private User getUser(){
+		return new User();
+	}
+}
+	
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<!--  <a href="WEB-INF/entryForm.jsp">Go to entry page</a>-->
+</body>
+</html>
